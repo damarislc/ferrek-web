5b9f04b4dcfdd6c4deddfd23aeadc050b7edb766
damarislc <damarislc9@gmail.com>
2024-05-13 21:36:13 -0400

    Añadiendo sweetalert para la entrega final

---
+++ js/index.js
@@ -0,0 +1,35 @@
+document.querySelector("#btn-truper").addEventListener("click", () => {
+  Swal.fire(
+    "Diseño duradero y potente motor, este taladro permite perforar con precisión en una variedad de materiales, desde madera y metal hasta concreto."
+  );
+});
+
+document.querySelector("#btn-clavos").addEventListener("click", () => {
+  Swal.fire("Clavos de acero de 2 pulgadas.");
+});
+
+document.querySelector("#btn-energizer").addEventListener("click", () => {
+  Swal.fire("4 baterías Energizer de lithium tipo AA.");
+});
+
+document.querySelector("#btn-pretul").addEventListener("click", () => {
+  Swal.fire(
+    "Kit de herramientas de Pretul, incluye 104 piezas multicolores para diversas aplicaciones mecánicas."
+  );
+});
+
+document.querySelector("#btn-tecnolite").addEventListener("click", () => {
+  Swal.fire("Lámpara tecnolite decorativa de techo.");
+});
+
+document.querySelector("#btn-broca").addEventListener("click", () => {
+  Swal.fire("Broca de concreto de 80 mm de largo por 14 mm de ancho.");
+});
+
+document.querySelector("#btn-brocha").addEventListener("click", () => {
+  Swal.fire("Brocha Perfect de 1 pulada y media.");
+});
+
+document.querySelector("#btn-desarmador").addEventListener("click", () => {
+  Swal.fire("Desarmador Truper punta cruz de 1/4 de pulgada.");
+});
